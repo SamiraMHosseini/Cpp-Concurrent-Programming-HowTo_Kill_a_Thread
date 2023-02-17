9c45114f38f0093cfc04a04b4ee029aa24706bba
SamiraMHosseini <62559858+SamiraMHosseini@users.noreply.github.com>
2023-02-16 19:54:34 -0600

    Add project files.

---
+++ SandBox/main.cpp
@@ -0,0 +1,70 @@
+#include <conio.h>
+struct SharedResource
+{
+	SharedResource() :
+		mtx(), flag(false)
+	{
+
+	}
+	//These are resources that are shared between 
+	std::mutex mtx;
+	std::condition_variable cv;
+	bool flag;
+};
+class A : public BannerBase
+{
+
+public:
+	A() = delete;
+	A(const A&) = default;
+	A& operator=(const A&) = default;
+	~A() = default;
+	A(const char* const name) :
+		counter(0), name(name), BannerBase(name)
+	{
+
+	}
+	void operator ()(SharedResource& sharedresource) //Functor 
+	{
+		START_BANNER;
+		Debug::SetCurrentName(name);
+		while (true)
+		{
+
+			std::unique_lock<std::mutex> lock(sharedresource.mtx);
+			if (sharedresource.cv.wait_for(lock, 500ms, [&]() -> bool { return sharedresource.flag; }))
+			{
+				break;
+			}
+
+			Debug::out("%d \n", counter);
+			++counter;
+
+
+		}
+
+	}
+
+private:
+	int counter;
+	const char* const name;
+};
+
+
+
+int main()
+{
+	START_BANNER_MAIN("Main");
+	A a("A");
+	SharedResource sharedresource;
+	std::thread thA(a, std::ref(sharedresource));
+	thA.detach();
+	_getch();
+	{
+		Debug::out("Key is pressed\n");
+		std::unique_lock<std::mutex> lock(sharedresource.mtx);
+		sharedresource.flag = true;
+		sharedresource.cv.notify_one();
+	}
+	std::this_thread::sleep_for(1s); //This is going to get fixed in other versions. 
+}
